b606a7c79f4e90af10cf6aab293c88b1a1a45d4f
UsmanKhanTaimure <107642292+UsmanKhanTaimure@users.noreply.github.com>
2022-06-16 10:46:21 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Chapter 2
+
+// Question_1
+// var FirstName = "Usman";
+// var lastName = " Khan" 
+// var fullName = FirstName+lastName;
+// document.write(fullName)
+
+// Question_2
+// var message = "Hello World";
+// alert(message)
+
+// Question_3
+// var UserName = "Usman";
+// var Age = 18;
+// var Skill = "Web And Mobile Development";
+// alert(UserName);
+// alert(Age);
+// alert(Skill);
+
+// Question_4
+// alert("PIZZA\n"+"PIZZ\n"+"PIZ\n"+"PI\n"+"P")
+
+// Question_5
+// var user = "My Email Address is";
+// var email = " Usmangeneious13@gamil.com";
+// var FullEmail = user+email;
+// document.write(FullEmail)
+
+// Question_6
+// alert("Iam trying to learn from the Book A smarter \nway to learn JavaScript")
+
+// Question_7
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
